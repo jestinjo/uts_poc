9498e5af6195415c2f49eb52e5788ce7164095c2
Jestin Jose <jestin.mec@gmail.com>
2020-08-14 20:38:30 +0530

    CLR

--- practise/src/app/app.module.ts
+++ practise/src/app/app.module.ts
@@ -4,13 +4,17 @@ import { AppComponent } from './app.component';
 import { MapTalksComponent } from './map-talks/map-talks.component';
 import { MapTalksPlainComponent } from './map-talks-plain/map-talks-plain.component';
 import { ClrDiagramComponent } from './clr-diagram/clr-diagram.component';
+import { CircuitLayerDiagramComponent } from './circuit-layer-diagram/circuit-layer-diagram.component';
+import { ClrGraphComponent } from './clr-graph/clr-graph.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MapTalksComponent,
     MapTalksPlainComponent,
-    ClrDiagramComponent
+    ClrDiagramComponent,
+    CircuitLayerDiagramComponent,
+    ClrGraphComponent,
   ],
   imports: [
     BrowserModule
